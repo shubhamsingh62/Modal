eb72987eb6afa61bbea5f5fcf6fd06a640b13f15
shubhamsingh62 <shubham.singh62@gmail.com>
2018-04-12 12:44:11 +0530

    First commit

---
+++ modal.js
@@ -0,0 +1,24 @@
+$(document).ready( () =>{
+
+     $('#btn3').click( () =>{
+    
+       $('.modal').fadeIn();
+
+
+     } )
+     $('.close').click( ()=>{
+
+       $('.modal').fadeOut();
+
+     } )
+     $('.btn1').click( ()=>{
+
+       $('.modal').fadeOut();
+
+     })
+     $('.btn2').click( ()=>{
+
+       $('.modal').fadeOut();
+     	
+     })
+})
